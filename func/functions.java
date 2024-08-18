078b0323049cedf648088e77eba5fc861e7669c3
Ahmed <ardyop7@gmail.com>
2024-08-18 13:24:28 +0000

    Just updated

--- func/functions.java
+++ func/functions.java
@@ -3,7 +3,7 @@ public class Functions{
   public static int returnMul(int a,int b){
     int mult = a * b;
     return mult;
-  }
+  }  
   
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
